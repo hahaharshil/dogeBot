1a9da74883a85981d8d390579bee6f74752d87bb
harshil <hahaharshil3006@gmail.com>
2021-06-10 15:08:29 +0530

    Basic structure on main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import asyncio
-from inspireAPI import get_quote
 import praw
 
 
@@ -10,6 +9,19 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+@client.event
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    message.content = message.content.lower()
+
+    if message.content.startswith('hello'):
+        await message.channel.send('Hello!')
+    if message.content.startswith("yo"):
+        await message.channel.send("yo?")
 
 
 client.run('NzcwNTg2NTgwNzI5MjAwNjgw.X5furQ.loybiSIUwbxl13x1-mejkHDIv78')
