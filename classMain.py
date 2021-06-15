1248f5598ac666992283244008d50425697608c9
Samarth <samarthdubey46@gmail.com>
2021-06-15 23:08:04 +0530

    added commands

--- classMain.py
+++ classMain.py
@@ -1,5 +1,5 @@
 import discord
-
+from message import getMessage
 client = discord.Client()
 
 
@@ -8,16 +8,18 @@ class Bot(discord.Client):
     async def on_ready(self):
         print(f'We have logged in as {client}')
 
-    async def on_message(self,message):
+    async def on_message(self, message):
         if message.author == client.user:
             return
-
         if message.content.startswith('hello'):
             await message.channel.send('Hello!')
         if message.content.startswith("yo"):
             await message.channel.send("yo?")
+        response = getMessage(message.content)
+        if response is not None:
+            await message.channel.send(response)
 
 
 if __name__ == "__main__":
     bot = Bot()
-    bot.run('ODU0MzE1MTY0Mjk0NTc4MjE2.YMiI_w.IELpy5woI2pSYMjxvchssZJclqI')
+    bot.run('NzcwNTg2NTgwNzI5MjAwNjgw.X5furQ.1X6RVwfkndymk3d-ZZKhzD1g0l4')
