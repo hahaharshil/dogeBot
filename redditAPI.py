5c81df59a28ba791f1f865f140866c382402bcf6
Samarth <samarthdubey46@gmail.com>
2021-06-20 23:40:46 +0530

    added requirements.txt

--- redditAPI.py
+++ redditAPI.py
@@ -16,11 +16,6 @@ def redditMemes():
         print(submission.url)
         return submission.url
 
-    # if type == "best":
-    #     best = subreddit.hot(limit=65)
-    #     best = list(best)
-    #     random_num = random.randint(0, len(best) - 1)
-    #     return best[random_num].url
 
 
 def cursedImages():
