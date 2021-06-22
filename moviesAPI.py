d511ea0c0f91ef6ccb93d24efe709d4c81208aa1
hahaharshil <hahaharshil@protonmail.com>
2021-06-22 23:20:26 +0530

    Completed

--- moviesAPI.py
+++ moviesAPI.py
@@ -1,18 +1,26 @@
-from re import I
 from imdb import IMDb
-from imdb.utils import analyze_title
 ia = IMDb()
 
-def IMDBmoviesi(name):
+
+
+
+def IMDBmovies(name):
     movies = ia.search_movie(name)
     title = (movies[0]["title"])
     year = (movies[0]["year"])
+
     id = (movies[0].movieID)
+
+    movie = ia.get_movie(id)
+    rating = movie.data["rating"]
+  
+    for x in movie['genres']:
+        genre = x
+
+
+
+    return title, year, rating, genre
+
     
-    print(analyze_title(title))
 
 
-    # return title, year, id 
-
-
-IMDBmoviesi("Race 3") 
