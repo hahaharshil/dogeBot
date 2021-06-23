9e4472de6f61d71410fceea2c5b9afcfb3908980
Samarth <samarthdubey46@gmail.com>
2021-06-23 23:43:38 +0530

    removed func calls and completed spotify

--- data.py
+++ data.py
@@ -1,8 +1,5 @@
-from redditAPI import redditMemes, cursedImages
-from spotify import featured_playlists,latest_albums
-
 commonGreetings = {
-    'hello': {'showAll': False, 'data': ['helloooo!!', 'nice to see you', 'hey! how are you today? ']},
+    'hello': {'showAll': False, 'data': ['helloooo!!', 'nice to see you', 'hey! how are you today?']},
     'help': {'showAll': True, 'data': ['Under development']},
     'What is your name': {'showAll': False, 'data': ['dogeBot']},
     'do you love me?': {'showAll': False, 'data': ['yes of Couse I love you !', 'you are so sweet I love you !',
@@ -22,14 +19,10 @@ commonGreetings = {
     'who do you work for?': {'showAll': False,
                              'data': ['idk never heard of you', 'you"re some cool brat: ', 'A mistake! I see.']},
     'let\'s have fun': {'showAll': False,
-                       'data': ['I am busy', "Let's go to mc Donald's", 'You tell me a joke', 'Like?']},
+                        'data': ['I am busy', "Let's go to mc Donald's", 'You tell me a joke', 'Like?']},
     'You are so annoying': {'showAll': False, 'data': ['I know ']},
     'what is the time?': {'showAll': False, 'data': ['Developing ']},
     'dogeBot': {'showAll': False, 'data': ['Developing ']},
-    'memes': {'showAll': False, 'useFunc': True, 'func': redditMemes,'param' : 'new', 'data': ['Developing ']},
-    'cursed memes': {'showAll': False, 'useFunc': True, 'func': cursedImages, 'param': 'new', 'data': ['Developing ']},
-    'albums': {'showAll': False, 'useFunc': True, 'func': latest_albums, 'param': 'new', 'data': ['Developing ']},
-    'playlists': {'showAll': False, 'useFunc': True, 'func': featured_playlists, 'param': 'new', 'data': ['Developing ']},
 }
 sampleData = [
     {'intent': 'happy', 'responses': []}
